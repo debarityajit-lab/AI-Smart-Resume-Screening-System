2745214cf0e2fd4cffbdccacf16c646360d1bf92
Subham Saha <subhamranjansaha006@gmail.com>
2026-03-18 19:44:04 +0530

    Few Final arrangements

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 app=Flask(__name__)
 CORS(app)
 
-genai.configure(api_key=os.environ.get("GEMINI_API_KEY","YOUR_API_KEY"))
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY","YOUR_GEMINI_KEY_HERE"))
 model=genai.GenerativeModel("gemini-2.5-flash")
 
 @app.route("/")
